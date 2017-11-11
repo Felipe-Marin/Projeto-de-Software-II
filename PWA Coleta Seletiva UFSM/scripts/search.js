84aac3a7323dcadce021eadfeb26a879d950c19c
Felipe-Marin <felipmarin5@gmail.com>
2017-11-11 18:48:10 -0200

    Correção no search.js
    código colocado dentro da método 'ready'

--- PWA Coleta Seletiva UFSM/scripts/search.js
+++ PWA Coleta Seletiva UFSM/scripts/search.js
@@ -1,29 +1,31 @@
 //Busca de resíduos
-var searchTable;
-var dataResiduos = {};
-$.ajax({
-    url: 'https://spreadsheets.google.com/feeds/list/1CNFUNPva91WPsoD2Yr0n3KQ4BjZcNkBRFhsYub1ClpA/od6/public/values?alt=json',
-    cache: true,
-    data: dataResiduos,
-    dataType: 'json',
-    success: function (data) {
-        searchTable = $('#table1').DataTable({
-            "responsive": true,
-            "dom": 'tp',
-            "data": data.feed.entry,
-            "columns": [
-                { "data": "gsx$residuo.$t" },
-                { "data": "gsx$tipo.$t" }
-            ]
-        });
-    }
-});
-$('#search').keyup(function () {
-    if ($('#search').val() == '') {
-        $('#results').hide();
-    }
-    else {
-        $('#results').show();
-        searchTable.search($(this).val()).draw();
-    }
+$(document).ready(function () {
+    var searchTable;
+    var dataResiduos = {};
+    $.ajax({
+        url: 'https://spreadsheets.google.com/feeds/list/1CNFUNPva91WPsoD2Yr0n3KQ4BjZcNkBRFhsYub1ClpA/od6/public/values?alt=json',
+        cache: true,
+        data: dataResiduos,
+        dataType: 'json',
+        success: function (data) {
+            searchTable = $('#table1').DataTable({
+                "responsive": true,
+                "dom": 'tp',
+                "data": data.feed.entry,
+                "columns": [
+                    { "data": "gsx$residuo.$t" },
+                    { "data": "gsx$tipo.$t" }
+                ]
+            });
+        }
+    });
+    $('#search').keyup(function () {
+        if ($('#search').val() == '') {
+            $('#results').hide();
+        }
+        else {
+            $('#results').show();
+            searchTable.search($(this).val()).draw();
+        }
+    });
 });
